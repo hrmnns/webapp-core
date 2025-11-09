996992af2dbe47e91149bb819f3a028257f533dc
Christoph Hermanns <hermanns@gmx.net>
2025-11-09 18:32:47 +0100

    Add files via upload
    Initial code base

---
+++ app.js
@@ -0,0 +1,4 @@
+// Hier können später Funktionen platziert werden, die in allen Bereichen genutzt werden.
+// Beispiel: Export als PDF, gemeinsame Validatoren, Logging, etc.
+
+console.log("Framework geladen");
